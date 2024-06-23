cc1731a98565d44a8ea7a2b1fdbdd4cfeb4de316
GG <qq1270414897@gmail.com>
2024-06-23 21:15:44 +0800

    ✨ feat(header): header init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,vue}","./index.html",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import forms from '@tailwindcss/forms'
 
+export default {
+  content: ['./src/**/*.{html,js,vue}', './index.html'],
+  theme: {
+    extend: {}
+  },
+  plugins: [forms]
+}
