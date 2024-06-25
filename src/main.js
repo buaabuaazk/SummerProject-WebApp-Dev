f7c7658c4574f4ed120a57538000ba7b72505bc5
aliyun0459792885-nakAm <1308199540@qq.com>
2024-06-25 16:37:15 +0800

    Revert no message
    This reverts commit 23e9383a

--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
-/*
- * @Author: aliyun0459792885-nakAm 1308199540@qq.com
- * @Date: 2024-06-25 16:14:38
- * @LastEditors: aliyun0459792885-nakAm 1308199540@qq.com
- * @LastEditTime: 2024-06-25 16:36:49
- * @FilePath: /frontend1/src/main.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 // import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import './assets/index.css'
@@ -28,7 +18,6 @@ pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
 app.config.globalProperties.$axios = axios
 app.config.globalProperties.$stores = stores
 app.config.globalProperties._ = _
