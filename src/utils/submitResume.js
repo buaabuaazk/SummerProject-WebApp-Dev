92b0dd38dc4a376bd456978fd0f60061b15a3425
Q9-K <20374319@buaa.edu.cn>
2024-06-27 14:32:54 +0800

    fiexed: 简历提交修复成功

--- src/utils/submitResume.js
+++ src/utils/submitResume.js
@@ -4,13 +4,22 @@
  * @Description: 提交简历接口
  */
 import axios from './request'
+import qs from 'qs'
+import commonToast from './commonToast'
 async function submitResume(job_id) {
   console.log('🚀 ~ file: submitResume.js:4 ~ submitResume ~ job_id:', job_id)
 
   try {
-    let res = await axios.post('/api/recruit/upload_resume', {
-      job_id
-    })
+    let res = await axios.post(
+      '/api/recruit/upload_resume',
+      qs.stringify({
+        recruit_id: job_id
+      })
+    )
+    const data = res.data
+
+    console.log('🚀 ~ file: submitResume.js:20 ~ submitResume ~ data:', data)
+    commonToast('success', 'Resume submitted successfully')
   } catch (error) {
     console.log('🚀 ~ file: submitResume.js:11 ~ submitResume ~ error:', error)
   }
