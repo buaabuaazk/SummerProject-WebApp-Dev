1a9db48daa8d48ef227f83e1764fdbed9c17273e
luogarden <1805044460@qq.com>
2024-07-02 13:59:30 +0800

    fix: 🐞修复私信bug

--- src/components/MessageBar.vue
+++ src/components/MessageBar.vue
@@ -16,6 +16,7 @@
     @mousedown="mouseDownHandler"
     ref="draggableContainer"
   >
+    <button @click="handleAppendMessage">添加消息</button>
     <NaiveChat
       ref="naiveChatRef"
       style="background-color: antiquewhite"
@@ -44,7 +45,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, watch } from 'vue'
+import { ref, onMounted, onUnmounted, watch, } from 'vue'
 import axios from '@/utils/request'
 import { NaiveChat } from 'naive-chat'
 import { onBeforeUnmount } from 'vue'
@@ -117,42 +118,42 @@ const stopDrag = () => {
   document.body.style.cursor = 'default'
 }
 
-const userInfo = {
+const userInfo = ref({
   id: currentUser.user_id,
   nickname: currentUser.username,
   avatar: currentUser.icon
-}
+})
 
 const contacts = ref([]) //联系人信息{id, avatar, nickname, lastmessage}
 
 const messages = ref([])
 
 const testMessages = {
-  id: 10,
-  content: 'test',
-  type: 'file',
+  id: 'id7',
+  content: '测试消息',
+  type: 'text',
   toContactId: 5,
   status: 'success',
-  sendTime: 1691056800000,
-  fileName: '艳萍简历.doc',
-  fileSize: 18238,
+  sendTime: Date.now(),
+  fileName: undefined,
   fromUser: {
     id: 1,
-    avatar:
-      'https://img.zcool.cn/community/0188be5f51d85811013e318730c7d8.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100'
+    nickname: 'Forgiving soldier',
+    avatar: 'http://rz1wa9fyb.hb-bkt.clouddn.com/liang.jpeg'
   }
 }
 
-const unreadCount = ref(0)
 const hasRead = ref([])
 
-const pullMessage = ({ next, contactId }) => {
-  if (!hasRead.value[contactId]) {
-    fetchOneMessageHistory(contactId)
-    asyncFn(() => {
-      next(messages.value, true)
-    })
-    hasRead.value[contactId] = true
+const pullMessage = async ({ next, contactId }) => {
+  if(contactId){
+    if (!hasRead.value[contactId]) {
+      await fetchOneMessageHistory(contactId)
+      asyncFn(() => {
+        next(messages.value, true)
+      })
+      hasRead.value[contactId] = true
+    }
   }
 }
 
@@ -162,36 +163,33 @@ const asyncFn = (fn) => {
   }, 1000)
 }
 
-const sendMessage = async (obj) => {
-  const { message, next } = obj
-  const response = await axios.post(
-    '/api/message/create',
-    {
-      id: message.id,
-      sender: message.fromUser.id,
-      toContactId: message.toContactId,
-      content: message.content,
-      type: message.type,
-      status: message.status
-    },
-    {
-      headers: {
-        Authorization: token
-      }
-    }
-  )
-  console.log(response.data)
-  if (response.status === 201) {
-    console.log('success')
-    asyncFn(() => {
-      next({
-        id: message.id,
-        toContactId: message.toContactId,
-        status: 'success'
-      })
-    })
-    messages.value.push(message)
-  }
+//发送消息
+const sendMessage = async ({ message, next }) => {
+  console.log({
+    front_id: message.id,
+    sender: userInfo.value.id,
+    toContactId: message.toContactId,
+    content: message.content,
+    type: message.type,
+    status: message.status,
+    fileName: undefined
+  })
+  const response = await axios.post('/api/message/create', {
+    front_id: message.id,
+    sender: message.fromUser.id,
+    toContactId: message.toContactId,
+    content: message.content,
+    type: message.type,
+    status: message.status,
+    fileName: undefined
+  })
+   asyncFn(() => {
+  next({
+    id: message.id,
+    toContactId: message.toContactId,
+    status: 'success'
+  })
+   })
 }
 
 //获取最新消息
@@ -202,7 +200,8 @@ const fetchLatestMessage = async () => {
         Authorization: token
       }
     })
-    contacts.value.push(...response.data.latest_messages)
+    contacts.value.push(response.data.latest_messages)
+    naiveChatRef.value?.appendMessage(response.data.latest_messages)
   } catch (err) {
     err.value = err.message
   }
@@ -211,11 +210,7 @@ const fetchLatestMessage = async () => {
 //获取与某人的聊天记录
 const fetchOneMessageHistory = async (toContactId) => {
   try {
-    const response = await axios.get(`/api/message/with/${toContactId}`, {
-      headers: {
-        Authorization: token
-      }
-    })
+    const response = await axios.get(`/api/message/with/${toContactId}`)
     messages.value = []
     messages.value = messages.value.concat(response.data)
   } catch (err) {
@@ -228,16 +223,10 @@ const fetchNewMessage = async (toContactId) => {
   try {
     if (toContactId !== undefined) {
       const number = String(toContactId)
-      const response = await axios.get(
-        'http://100.111.110.112:8000/api/message/latest/' + `${number}`,
-        {
-          headers: {
-            Authorization: token
-          }
-        }
-      )
-      console.log(response.data)
-      if (isNewMessage.value !== response.data.id && userInfo.id !== response.data.sender) {
+      console.log(userInfo.value.id)
+      const response = await axios.get('/api/message/latest/1')
+      console.log(response)
+      if (isNewMessage.value !== response.data.id && userInfo.value.id !== response.data.sender) {
         isNewMessage.value = response.data.id
         naiveChatRef.value?.appendMessage(response.data)
       }
@@ -251,42 +240,24 @@ const fetchNewMessage = async (toContactId) => {
 
 const fetchNewMessageTrigger = async () => {
   if (naiveChatRef.value?.getCurrentContact()?.id !== undefined) {
-    console.log(naiveChatRef.value?.getCurrentContact()?.id)
     await fetchNewMessage(naiveChatRef.value?.getCurrentContact()?.id)
   }
 }
 
-//有未读消失时给出提示
-// const fetchTotalUnreadMessageNumber = async () => {
-//   try {
-//     const response = await axios.get('/api/message/unread-count', {
-//       headers: {
-//         'Authorization': token
-//       }
-//     })
-//     if (response.data.unread > unreadCount.value ) {
-//       unreadCount.value = response.data.unread_count
-
-//       //console.log(naiveChatRef.value?.getCurrentUser)
-//     }
-//   } catch (err){
-//     console.log(err)
-//   }
-// }
-
 const openContact = async () => {
-  const flag = ref(false)
+  let flag = false
+  console.log(contacts.value)
   for (const item of contacts.value) {
+    console.log(item.id, Number(messageStore.toContactId))
     if (Number(item.id) === Number(messageStore.toContactId)) {
-      flag.value = true
-      console.log('equal')
+      flag = true
       break
     }
   }
-  if (flag.value) {
+  if (flag) {
     console.log('cant do this')
   } else {
-    console.log(flag.value)
+    console.log(flag)
     const response = await axios.get('/api/user/info', {
       params: {
         user_id: messageStore.toContactId
@@ -299,68 +270,84 @@ const openContact = async () => {
       lastMessage: '打个招呼吧',
       lastTime: Date.now()
     }
-    console.log(response.data.username)
     contacts.value.unshift(newContact)
-    console.log(contacts.value)
     naiveChatRef.value?.initContacts(contacts.value)
+    //naiveChatRef.value?.appendMessage(testMessages)
   }
 }
 
+const handleAppendMessage = () => {
+  const msg = {
+    id: 500,
+    content: '最近忙什么呢?',
+    type: 'text',
+    toContactId: 5,
+    fromUser: {
+      id: 1,
+      nickname: 'Forgiving soldier',
+      avatar: 'http://rz1wa9fyb.hb-bkt.clouddn.com/liang.jpeg'
+    },
+    sendTime: Date.now(),
+    status: 'success',
+    fileName: undefined
+  }
+  naiveChatRef.value?.appendMessage(msg)
+}
+let interval
 onMounted(async () => {
   document.addEventListener('mouseup', stopDrag)
-  fetchLatestMessage()
+  await fetchLatestMessage()
+  console.log(userInfo.value)
   naiveChatRef.value?.initContacts(contacts.value)
-  naiveChatRef.value?.appendMessage(testMessages)
   openContact()
-  // const interval = setInterval(fetchNewMessageTrigger, 2000)
+  interval = setInterval(fetchNewMessageTrigger, 2000)  
+})
 
-  // //清除定时器以避免内存泄漏
-  // onUnmounted(() => {
-  //   clearInterval(interval)
-  // })
+onUnmounted(() => {
+    clearInterval(interval)
 })
 
 onBeforeUnmount(() => {
   document.removeEventListener('mouseup', stopDrag)
 })
 
-watch(
-  () => messageStore.sendMessageTriggered,
-  async () => {
-    const flag = ref(false)
-    console.log('problem')
-    for (const item of contacts.value) {
-      console.log(item.id)
-      console.log(messageStore.toContactId)
-      console.log(typeof item.id, item.id) // 输出类型和值
-      console.log(typeof messageStore.toContactId, messageStore.toContactId)
-      if (Number(item.id) === Number(messageStore.toContactId)) {
-        flag.value = true
-        console.log('equal')
-        break
-      }
-    }
-    if (flag.value === false) {
-      console.log(flag.value)
-      const response = await axios.get('/api/user/info', {
-        params: {
-          user_id: messageStore.toContactId
-        }
-      })
-      const newContact = {
-        id: response.data.user_id,
-        nickname: response.data.username,
-        avatar: response.data.icon,
-        lastMessage: '打个招呼吧',
-        lastTime: Date.now()
-      }
-      console.log(response.data.username)
-      contacts.value.unshift(newContact)
-      console.log(contacts.value)
-      naiveChatRef.value?.initContacts(contacts.value)
-    }
-  }
-)
+// watch(
+//   () => messageStore.sendMessageTriggered,
+//   async () => {
+//     const flag = ref(false)
+//     console.log('problem')
+//     for (const item of contacts.value) {
+//       console.log(item.id)
+//       console.log(messageStore.toContactId)
+//       console.log(typeof item.id, item.id) // 输出类型和值
+//       console.log(typeof messageStore.toContactId, messageStore.toContactId)
+//       if (Number(item.id) === Number(messageStore.toContactId)) {
+//         flag.value = true
+//         console.log('equal')
+//         break
+//       }
+//     }
+//     if (flag.value === false) {
+//       console.log(flag.value)
+//       const response = await axios.get('/api/user/info', {
+//         params: {
+//           user_id: messageStore.toContactId
+//         }
+//       })
+//       const newContact = {
+//         id: response.data.user_id,
+//         nickname: response.data.username,
+//         avatar: response.data.icon,
+//         lastMessage: '打个招呼吧',
+//         lastTime: Date.now()
+//       }
+//       console.log(response.data.username)
+//       contacts.value.unshift(newContact)
+//       console.log(contacts.value)
+//       naiveChatRef.value?.initContacts(contacts.value)
+//     }
+//   }
+// )
 </script>
 
 <style scoped>
